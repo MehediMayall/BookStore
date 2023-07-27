28f9eaee18684686341247c60e8bf1262d692d19
mehedi <mehedisun@outlook.com>
2023-07-27 20:49:45 +0600

    created webapi project

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,22 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BookStore.models;
-using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
 
-using (var db = new BookContext())
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    foreach(var book in db.Books.AsNoTracking().Include(x => x.Author))
-    {
-        Console.WriteLine($"Name: {book.Title}, Author: {book.Author.Name}");
-    }
-
-    var mybook = db.Books.Include(a=> a.Author).Single(b=> b.Title == "Domain Driven Design");
-    mybook.PublishedOn = new DateTime(2017, 5, 20);
-    mybook.Author.Email = "EricEvans@gmail.com";
-    db.SaveChanges();
-
-    Console.WriteLine($"Title: {mybook.Title}, Author: {mybook.Author.Name}, Published Date: {mybook.PublishedOn.ToShortDateString()}");
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-Console.WriteLine("<- end of the programe ->");
+app.UseHttpsRedirection();
 
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
