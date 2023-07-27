c175b1d1591a71af85f68de643d712139e62c4c0
mehedi <mehedisun@outlook.com>
2023-07-27 18:16:31 +0600

    last update

---
+++ BookStore/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BookStore.models;
+using Microsoft.EntityFrameworkCore;
+
+using (var db = new BookContext())
+{
+    foreach(var book in db.Books.AsNoTracking().Include(x => x.Author))
+    {
+        Console.WriteLine($"Name: {book.Title}, Author: {book.Author.Name}");
+    }
+
+    var mybook = db.Books.Include(a=> a.Author).Single(b=> b.Title == "Domain Driven Design");
+    mybook.PublishedOn = new DateTime(2017, 5, 20);
+    mybook.Author.Email = "EricEvans@gmail.com";
+    db.SaveChanges();
+
+    Console.WriteLine($"Title: {mybook.Title}, Author: {mybook.Author.Name}, Published Date: {mybook.PublishedOn.ToShortDateString()}");
+
+}
+
+Console.WriteLine("<- end of the programe ->");
+
---
+++ BookStore/models/Book.cs
@@ -0,0 +1,11 @@
+namespace BookStore.models;
+
+public class Book {
+    public int BookID { get; set;}
+    public string Title { get; set;}
+    public string Description { get; set; }
+    public DateTime PublishedOn { get; set; }
+
+    public int AuthorID { get; set; }
+    public Author Author { get; set; }
+}
